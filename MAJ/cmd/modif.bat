1482d2184021150f33293bd6d508a56570b0a1af
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2021-11-05 16:01:15 +0100

    Update modif.bat

--- MAJ/cmd/modif.bat
+++ MAJ/cmd/modif.bat
@@ -1,16 +1,4 @@
 @echo off
-Color 0A & Mode con cols=40 lines=2
-@echo Suppression des fichiers obsoletes...
-@echo off
-timeout /t 3 /nobreak > nul
-Color 0A & Mode con cols=27 lines=2
-@echo Suppression terminer
-@echo off
-timeout /t 3 /nobreak > nul
-cd..
-cd..
-Color 0b & Mode con cols=25 lines=2
-@echo Demarrage launcher ok
-Launcher.exe
+
 exit
 
