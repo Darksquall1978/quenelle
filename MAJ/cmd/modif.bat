688aeb56aaef178f4c94ff8875c458202c5dbbb0
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2021-11-05 15:40:14 +0100

    Add files via upload

--- MAJ/cmd/modif.bat
+++ MAJ/cmd/modif.bat
@@ -1,2 +1,8 @@
-@echo ok...
-pause
+@echo off
+Color 0A & Mode con cols=40 lines=2
+@echo Suppression des fichiers obsoletes...
+@echo off
+timeout /t 3 /nobreak > nul
+@echo Suppression terminer
+@echo off
+timeout /t 3 /nobreak > nul
