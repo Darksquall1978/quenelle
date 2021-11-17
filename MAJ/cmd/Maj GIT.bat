9b22db4ed5a169d7ea8fa0f34e2ecec6fa714613
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2021-11-17 20:30:55 +0100

    Update Maj GIT.bat

--- MAJ/cmd/Maj GIT.bat
+++ MAJ/cmd/Maj GIT.bat
@@ -1,11 +1,13 @@
 @echo off
 Color 0e & Mode con cols=100 lines=40
 
-git remote add valheim git://github.com/Darksquall1978/quenelle.git
+::git remote add valheim git://github.com/Darksquall1978/quenelle.git
 
-::git fetch valheim
+git://github.com/Darksquall1978/quenelle.git
 
-::git fetch
+git fetch valheim
+
+git fetch
 
 git pull
 
