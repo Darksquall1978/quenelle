91029971854a43c2bc8de54381fa83bf38c684d8
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-05 18:11:05 +0100

    Add files via upload

--- MAJ/cmd/clone.bat
+++ MAJ/cmd/clone.bat
@@ -5,52 +5,26 @@ git clone https://github.com/Darksquall1978/quenelle.git
 cd..
 cd..
 @echo off
-@echo  suppression du .git
 RMDIR /s /q .git
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier cache
 RMDIR /s /q bepinex\cache
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier core
 RMDIR /s /q bepinex\core
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier patchers
 RMDIR /s /q bepinex\patchers
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier plugins
-RMDIR /s /q bepinex\plugins
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier config
-RMDIR /s /q bepinex\config
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier plugins-off
-RMDIR /s /q bepinex\plugins-off
-
-@echo  clonage des fichiers source
-
+::RMDIR /s /q bepinex\plugins
+::RMDIR /s /q bepinex\plugins-off
 timeout /t 2 /nobreak > nul
 
 
-
 @echo off
-robocopy maj\cmd\quenelle\.git .git /mt /z /e
- timeout /t 1 /nobreak > nul
+robocopy maj\cmd\quenelle\.git .git /mt /z /e 
+robocopy maj\cmd\quenelle\bepinex\cache BepInEx\cache
 robocopy maj\cmd\quenelle\bepinex\core BepInEx\core
-timeout /t 1 /nobreak > nul
-robocopy maj\cmd\quenelle\bepinex\core BepInEx\cache
-timeout /t 1 /nobreak > nul
 robocopy maj\cmd\quenelle\bepinex\patchers BepInEx\patchers
-timeout /t 1 /nobreak > nul
 robocopy maj\cmd\quenelle\bepinex\plugins BepInEx\plugins
-timeout /t 1 /nobreak > nul
-robocopy maj\cmd\quenelle\bepinex\config BepInEx\config
-timeout /t 1 /nobreak > nul
 robocopy maj\cmd\quenelle\bepinex\plugins-off BepInEx\plugins-off
-timeout /t 1 /nobreak > nul
-::copy maj\cmd\quenelle\maj\cmd\clone.bat maj\cmd\clone.bat
-::copy maj\cmd\quenelle\maj\cmd\modif.bat maj\cmd\modif.bat
-::copy maj\cmd\quenelle\maj\cmd\reparer.bat maj\cmd\reparer.bat
-::copy maj\cmd\quenelle\maj\cmd\reparerfin.bat maj\cmd\reparerfin.bat
+copy maj\cmd\quenelle\maj\cmd\clone.bat maj\cmd\clone.bat
+copy maj\cmd\quenelle\maj\cmd\modif.bat maj\cmd\modif.bat
+copy maj\cmd\quenelle\maj\cmd\reparer.bat maj\cmd\reparer.bat
+copy maj\cmd\quenelle\maj\cmd\reparerfin.bat maj\cmd\reparerfin.bat
 
 
 @echo off
@@ -59,7 +33,7 @@ RMDIR /s /q maj\cmd\quenelle
 start maj\cmd\reparerfin.bat
 
 exit
-
+.
 
 
 @echo off ---------------------------temp---------------------------
@@ -76,5 +50,3 @@ exit
 ::move /y maj\cmd\valheim\launcher.apm
 ::move /y maj\cmd\valheim\launcher.exe
 ::move /y maj\cmd\valheim\winhttp_.dll
-
-::confirme
