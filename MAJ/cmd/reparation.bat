ffa40648ac1b724086373e428bca3d802fc7930f
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-12 14:03:34 +0100

    Add files via upload

--- MAJ/cmd/reparation.bat
+++ MAJ/cmd/reparation.bat
@@ -17,11 +17,11 @@ timeout /t 1 /nobreak > nul
 @echo  suppression du dossier patchers
 RMDIR /s /q bepinex\patchers
 timeout /t 1 /nobreak > nul
-@echo  suppression du dossier plugins
-RMDIR /s /q bepinex\plugins
-timeout /t 1 /nobreak > nul
-@echo  suppression du dossier config
-RMDIR /s /q bepinex\config
+::@echo  suppression du dossier plugins
+::RMDIR /s /q bepinex\plugins
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier config
+::RMDIR /s /q bepinex\config
 timeout /t 1 /nobreak > nul
 @echo  suppression du dossier plugins-off
 RMDIR /s /q bepinex\plugins-off
@@ -38,9 +38,8 @@ timeout /t 1 /nobreak > nul
 @echo  suppression du dossier vplus-data
 RMDIR /s /q vplus-data
 timeout /t 1 /nobreak > nul
-::@echo  suppression du fichier doorstop_config.ini
-::del /s /q doorstop_config.ini
-::timeout /t 1 /nobreak > nul
+
+::pause
 
 @echo  clonage des fichiers source
 
@@ -53,27 +52,29 @@ robocopy maj\cmd\quenelle\.git .git /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
-robocopy maj\cmd\quenelle\bepinex\core BepInEx\core
+robocopy maj\cmd\quenelle\bepinex\core BepInEx\core /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
-robocopy maj\cmd\quenelle\bepinex\cache BepInEx\cache
+robocopy maj\cmd\quenelle\bepinex\cache BepInEx\cache /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
-robocopy maj\cmd\quenelle\bepinex\patchers BepInEx\patchers
+robocopy maj\cmd\quenelle\bepinex\patchers BepInEx\patchers /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
+xcopy maj\cmd\quenelle\bepinex\plugins BepInEx\plugins /y
+timeout /t 1 /nobreak > nul
 robocopy maj\cmd\quenelle\bepinex\plugins BepInEx\plugins /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+xcopy maj\cmd\quenelle\bepinex\config BepInEx\config /y
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
-robocopy maj\cmd\quenelle\bepinex\config BepInEx\config
-timeout /t 1 /nobreak > nul
-::ok
-::pause
-robocopy maj\cmd\quenelle\bepinex\plugins-off BepInEx\plugins-off
+robocopy maj\cmd\quenelle\bepinex\plugins-off BepInEx\plugins-off /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
@@ -93,10 +94,22 @@ robocopy maj\cmd\quenelle\vplus-data vplus-data /mt /z /e
 timeout /t 1 /nobreak > nul
 ::ok
 ::pause
-copy maj\cmd\quenelle\doorstop_config.ini doorstop_config.ini /y
+xcopy maj\cmd\quenelle\doorstop_config.ini doorstop_config.ini /y
 timeout /t 1 /nobreak > nul
+::ok
+::pause
 robocopy maj\cmd\quenelle\maj\cmd maj\cmd /mt /z /e 
 timeout /t 1 /nobreak > nul
+::ok
+::pause
+xcopy maj\cmd\quenelle\Launcher.apm Launcher.apm /y
+timeout /t 1 /nobreak > nul
+::ok
+::pause
+xcopy maj\cmd\quenelle\winhttp_.dll winhttp_.dll /y
+timeout /t 1 /nobreak > nul
+::ok
+::pause
 
 
 ::---------------------------------------------------------------------
