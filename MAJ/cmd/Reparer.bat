b190e1cc8e41e1546fcedf5ca76cc7e622a0d360
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-13 13:39:48 +0100

    Update Reparer.bat

--- MAJ/cmd/Reparer.bat
+++ MAJ/cmd/Reparer.bat
@@ -1,11 +1,13 @@
 Color 0c & Mode con cols=25 lines=2
-::@echo Suppression du clone...
+
 @echo off
+git pull
 ::timeout /t 3 /nobreak > nul
 
 ::RMDIR /s /q quenelle
 ::@echo Suppression ok...
 @echo off
+
 ::timeout /t 3 /nobreak > nul
 
 @echo Reparation en cours...
