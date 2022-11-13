a4b040b987e0246c0cdf34de2c3f0e3a4a222258
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-13 13:41:54 +0100

    Update Reparer.bat

--- MAJ/cmd/Reparer.bat
+++ MAJ/cmd/Reparer.bat
@@ -1,19 +1,11 @@
 Color 0c & Mode con cols=25 lines=2
-
-@echo off
 git pull
-::timeout /t 3 /nobreak > nul
-
-::RMDIR /s /q quenelle
-::@echo Suppression ok...
-@echo off
-
-::timeout /t 3 /nobreak > nul
+timeout /t 1 /nobreak > nul
 
 @echo Reparation en cours...
 timeout /t 3 /nobreak > nul
 reparation.bat 
 exit
-.
+
 
 
