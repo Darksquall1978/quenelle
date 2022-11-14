eecc8502a9eb2e0be6130d4f6c05374becb08509
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-14 16:59:32 +0100

    Update Reparer.bat

--- MAJ/cmd/Reparer.bat
+++ MAJ/cmd/Reparer.bat
@@ -2,7 +2,7 @@ Color 0c & Mode con cols=25 lines=2
 git pull
 timeout /t 1 /nobreak > nul
 
-@echo Reparation en cours...
+@echo Reparation en cours....
 timeout /t 3 /nobreak > nul
 reparation.bat 
 exit
