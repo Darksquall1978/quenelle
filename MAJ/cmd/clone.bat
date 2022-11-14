8b4f613fcbf2ce1023edff516db05c39bc2161da
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-14 20:51:39 +0100

    Update clone.bat

--- MAJ/cmd/clone.bat
+++ MAJ/cmd/clone.bat
@@ -19,11 +19,11 @@ RMDIR /s /q bepinex\core
 @echo  suppression du dossier patchers
 RMDIR /s /q bepinex\patchers
 ::timeout /t 1 /nobreak > nul
-::@echo  suppression du dossier plugins
-::RMDIR /s /q bepinex\plugins
+@echo  suppression du dossier plugins
+RMDIR /s /q bepinex\plugins
 ::timeout /t 1 /nobreak > nul
-::@echo  suppression du dossier config
-::RMDIR /s /q bepinex\config
+@echo  suppression du dossier config
+RMDIR /s /q bepinex\config
 ::timeout /t 1 /nobreak > nul
 @echo  suppression du dossier plugins-off
 RMDIR /s /q bepinex\plugins-off
@@ -40,20 +40,20 @@ timeout /t 1 /nobreak > nul
 @echo  suppression du dossier vplus-data
 RMDIR /s /q vplus-data
 timeout /t 1 /nobreak > nul
-@echo  suppression du fichier ConfigurationManager.xml
-RMDIR /s /q plugins/ConfigurationManager.xml
-@echo  suppression du fichier FloatingItems.dll
-RMDIR /s /q plugins/FloatingItems.dll
-@echo  suppression du fichier manifest.json
-RMDIR /s /q plugins/manifest.json
-@echo  suppression du fichier CW_Jesse.BetterNetworking.dll
-RMDIR /s /q plugins/CW_Jesse.BetterNetworking.dll
-@echo  suppression du fichier DeathTweaks.dll
-RMDIR /s /q plugins/DeathTweaks.dll
-@echo  suppression du fichier ExtendedItemDataFramework.dll
-RMDIR /s /q plugins/ExtendedItemDataFramework.dll
-@echo  suppression du fichier Jotunn.dll
-RMDIR /s /q plugins/Jotunn.dll
+::@echo  suppression du fichier ConfigurationManager.xml
+::RMDIR /s /q plugins/ConfigurationManager.xml
+::@echo  suppression du fichier FloatingItems.dll
+::RMDIR /s /q plugins/FloatingItems.dll
+::@echo  suppression du fichier manifest.json
+::RMDIR /s /q plugins/manifest.json
+::@echo  suppression du fichier CW_Jesse.BetterNetworking.dll
+::RMDIR /s /q plugins/CW_Jesse.BetterNetworking.dll
+::@echo  suppression du fichier DeathTweaks.dll
+::RMDIR /s /q plugins/DeathTweaks.dll
+::@echo  suppression du fichier ExtendedItemDataFramework.dll
+::RMDIR /s /q plugins/ExtendedItemDataFramework.dll
+::@echo  suppression du fichier Jotunn.dll
+::RMDIR /s /q plugins/Jotunn.dll
 
 ::pause
 
