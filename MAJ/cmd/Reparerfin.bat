14208f8cd2ccc6d5184c42e768ab500bd08a6d53
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-15 23:59:51 +0100

    Update Reparerfin.bat

--- MAJ/cmd/Reparerfin.bat
+++ MAJ/cmd/Reparerfin.bat
@@ -1,14 +1,7 @@
 Color 0c & Mode con cols=50 lines=50
-@echo Reparation ok 
-@echo off 
-::timeout /t 5 /nobreak > nul
-@echo off 
-
-::timeout /t 2 /nobreak > nul
 
 ::launcher.exe
-dir
-pause
+
 start MAJ\cmd\Netoyage.bat
 
 exit
