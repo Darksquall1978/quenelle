a56530d82c115d35a94232f9c7aec48a3f1aeea7
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-15 17:56:55 +0100

    Update Netoyage.bat

--- MAJ/cmd/Netoyage.bat
+++ MAJ/cmd/Netoyage.bat
@@ -2,18 +2,17 @@
 Color 0A & Mode con cols=120 lines=100
 
 
-dir
-pause
+
 @echo  suppression du dossier plugins
 @echo off
 RMDIR /s /q bepinex\plugins
-pause
+::pause
 ::timeout /t 1 /nobreak > nul
 
 @echo  suppression du dossier config
 @echo off
 RMDIR /s /q bepinex\config
-pause
+::pause
 MAJ/cmd/reparation.bat
 
 exit
