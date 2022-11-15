abd6a4f4447be94f14452a97d44bd972e0f89f62
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-15 16:56:22 +0100

    Update Clonage.bat

--- MAJ/cmd/Clonage.bat
+++ MAJ/cmd/Clonage.bat
@@ -3,8 +3,10 @@
 @echo off
 Color 0A & Mode con cols=120 lines=100
 git clone https://github.com/Darksquall1978/quenelle.git 
-
-@echo reparation en cours....
+@echo Clonage terminer....
+@echo off
+timeout /t 1 /nobreak > nul
+@echo Reparation en cours....
 @echo off
 timeout /t 1 /nobreak > nul
 reparation.bat
