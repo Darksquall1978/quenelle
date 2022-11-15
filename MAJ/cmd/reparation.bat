dd230e4a19b8e8f5cb9af6f2edc62ba9757711d6
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-15 18:19:41 +0100

    Update reparation.bat

--- MAJ/cmd/reparation.bat
+++ MAJ/cmd/reparation.bat
@@ -3,7 +3,9 @@ Color 0A & Mode con cols=120 lines=100
 git clone https://github.com/Darksquall1978/quenelle.git 
 
 cd..
-cd..
+dir
+pause
+::cd..
 @echo off
 @echo MAJ du --12/11/2022-- 14H30
 timeout /t 2 /nobreak > nul
