c321922b8c19a159c0f9cab2beeaadefc145d312
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-11-29 18:10:45 +0100

    Update Maj GIT.bat

--- MAJ/cmd/Maj GIT.bat
+++ MAJ/cmd/Maj GIT.bat
@@ -2,25 +2,16 @@
 Color 0e & Mode con cols=100 lines=40
 
 ::git remote add valheim git://github.com/Darksquall1978/quenelle.git
-
-
-
 ::git fetch valheim
-
 ::git fetch
 
 git pull
 
-::@echo Mise a jour OK
-timeout /t 3 /nobreak > nul
+timeout /t 1 /nobreak > nul
 
 ::RMDIR /s /q quenelle
 
-
-
-::if exist "temp.bat"
 start temp.bat
-
 
 ::start modif.bat
 
